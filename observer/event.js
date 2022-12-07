aa5f4914d68f372ce35d2f044dbc65340920cc7d
Alejandro Chazarreta <lcrender@gmail.com>
2022-12-07 14:39:22 +0100

    limpieza archivos inncesesarios

--- observer/event.js
+++
@@ -1,9 +0,0 @@
-const EventEmitter = require("events")
-const emitter = new EventEmitter();
-
-emitter.on("messageLogged", (arg) => {
-    console.log("listener called", arg);
-});
-
-emitter.emit("messageLogged", {});
-
